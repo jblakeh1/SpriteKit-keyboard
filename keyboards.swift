4c69a29d106570b21f54c72f49deb4272d09fa8c
J. Blake Harris <hello@motorcycl3.com>
2023-09-25 16:29:55 -0500

    Add files via upload

---
+++ keyboards.swift
@@ -0,0 +1,64 @@
+import GameController
+import SpriteKit
+
+
+extension GameScene {
+	
+	func pollInput() {
+		#if os(iOS) || os(tvOS)
+        if let keyboard = GCKeyboard.coalesced?.keyboardInput {
+            if (keyboard.button(forKeyCode: .keyA)?.isPressed ?? false) {
+                //do stuff
+            }
+        }
+		#endif
+	}
+	
+	#if os(macOS)
+	
+	// mac keyboard is not recognized as a game controller
+	public override func keyDown(with event: NSEvent) {
+		if event.modifierFlags.contains(NSEvent.ModifierFlags.numericPad)  {
+			let keyCode = event.keyCode
+			
+			// left arrow
+			if keyCode == 123 {
+				if controlPanel.alpha == 0 {
+					stopElevators()
+					animateFloorRotation(direction: 0)
+				}
+			}
+			
+			// right arrow
+			if keyCode == 124 {
+                //do stuff
+			}
+
+			// down arrow
+			if keyCode == 125 {
+                //do stuff
+			}
+			
+			// down arrow
+			if keyCode == 126 {
+                //do stuff
+			}
+		}
+		 else {
+			if let characters = event.characters{
+				for character in characters {
+					switch character {
+					case "a":
+						// do stuff
+						break
+
+					default:
+						break
+					}
+				}
+			}
+		}
+	}
+
+	#endif
+}
